9ed1aea6e0181851a5207134bc0ab661d5db1e12
Hubert Berja <bert.stark3231@gmail.com>
2023-03-22 20:39:35 +0800

    added router 3

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const jsonServer = require("json-server");
 const fs = require("fs");
 const path = require("path");
 const server = jsonServer.create();
-const db = JSON.stringify(fs.writeFileSync(path.join("/tmp", "db.json")));
+const db = JSON.parse(fs.readFileSync(path.join("/tmp", "db.json")));
 const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
 
@@ -16,7 +16,7 @@ server.use(
   })
 );
 server.use(router);
-server.listen(8000, () => {
+server.listen(3001, () => {
   console.log("JSON Server is running");
 });
 
